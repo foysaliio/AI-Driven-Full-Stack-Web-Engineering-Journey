4f134287aae805de0121a5804591cc4b35665443
Foysal Hossien <foysalhossien77@gmail.com>
2026-07-27 00:33:34 +0600

    feat(js): Count Vowels in a String

--- Java Script/Basic Problem Solving with JavaScript/problem-6.js
+++ Java Script/Basic Problem Solving with JavaScript/problem-6.js
@@ -1,2 +1,19 @@
 // Problem 6: Count Vowels in a String
 
+function countVowels(word) {
+  let vowels = "aeiou";
+  let count = 0;
+  for (let i = 0; i < word.length; i++) {
+    let char = word[i];
+    if (vowels.includes(char)) {
+      count++;
+    }
+  }
+  return count;
+}
+
+let phVowelCount = countVowels("Programming Hero");
+console.log(phVowelCount);
+
+let bdVowelCount = countVowels("Bangladesh");
+console.log(bdVowelCount);
