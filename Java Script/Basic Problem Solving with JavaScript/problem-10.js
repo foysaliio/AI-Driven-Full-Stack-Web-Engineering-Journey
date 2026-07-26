eef72e5cfbe2c7bbf148bf38b3296a31225f7b1a
Foysal Hossien <foysalhossien77@gmail.com>
2026-07-27 02:13:07 +0600

    feat(js): Find the Smallest Number in an Array

--- Java Script/Basic Problem Solving with JavaScript/problem-10.js
+++ Java Script/Basic Problem Solving with JavaScript/problem-10.js
@@ -1,2 +1,13 @@
 // Problem 10: Find the Smallest Number in an Array
 
+function smallestNumberFromArray(arr) {
+  let smallest = arr[0];
+  for (const number of arr) {
+    if (number < smallest) {
+      smallest = number;
+    }
+  }
+  return smallest;
+}
+
+console.log(smallestNumberFromArray([250, 20, 5, 732, 64, 2, 560]));
