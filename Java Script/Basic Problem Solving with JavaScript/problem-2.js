e52ab2b716e83f6e640e965c754f71aad17d3f8e
Foysal Hossien <foysalhossien77@gmail.com>
2026-07-26 23:57:29 +0600

    feat(js): Sum of a Range of Numbers (1 to N)

--- Java Script/Basic Problem Solving with JavaScript/problem-2.js
+++ Java Script/Basic Problem Solving with JavaScript/problem-2.js
@@ -1,2 +1,12 @@
 // Problem 2: Sum of a Range of Numbers (1 to N)
 
+function sumRange(number) {
+  let sum = 0;
+  for (let i = 1; i <= number; i++) {
+    sum = sum + i;
+  }
+  return sum;
+}
+
+console.log(sumRange(7));
+console.log(sumRange(4));
