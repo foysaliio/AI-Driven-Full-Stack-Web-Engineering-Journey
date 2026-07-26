08c67d3b523164e3686a8462e920a074571a60c5
Foysal Hossien <foysalhossien77@gmail.com>
2026-07-27 03:04:14 +0600

    feat(js): Sum and Average of an Array

--- Java Script/Basic Problem Solving with JavaScript/problem-11.js
+++ Java Script/Basic Problem Solving with JavaScript/problem-11.js
@@ -1,2 +1,17 @@
 // Problem 11: Sum and Average of an Array
 
+function generateAvg(arr) {
+  let sum = 0;
+
+  for (number of arr) {
+    sum += number;
+  }
+
+  let avg = sum / arr.length;
+
+  return avg;
+}
+
+console.log(generateAvg([5, 10, 15]));
+console.log(generateAvg([1, 2, 3]));
+console.log(generateAvg([3, 23, 10]));
