4ea2589b2313b515a6634ff4912e4c26fde5425d
Foysal Hossien <foysalhossien77@gmail.com>
2026-07-27 00:41:59 +0600

    feat(js): Count Words in a Sentence

--- Java Script/Basic Problem Solving with JavaScript/problem-8.js
+++ Java Script/Basic Problem Solving with JavaScript/problem-8.js
@@ -1,2 +1,10 @@
 // Problem 8: Count Words in a Sentence
 
+function wordCount(sentence) {
+  let sentenceSplitArr = sentence.split(" ");
+  return sentenceSplitArr.length;
+}
+
+console.log(wordCount("Hello Bangladesh"));
+console.log(wordCount("I love Bangladesh"));
+console.log(wordCount('My name is Foysal Hossien'));
