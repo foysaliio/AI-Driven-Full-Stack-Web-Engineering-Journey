141515fbe610088fb393658711e5e3a7b1c120cd
Foysal Hossien <foysalhossien77@gmail.com>
2026-07-27 02:11:01 +0600

    feat(js): Find the Largest Number in an Array

--- Java Script/Basic Problem Solving with JavaScript/problem-9.js
+++ Java Script/Basic Problem Solving with JavaScript/problem-9.js
@@ -1,2 +1,13 @@
 // Problem 9: Find the Largest Number in an Array
 
+function largestNumberFromArray(arr) {
+  let largest = arr[0];
+  for (const number of arr) {
+    if (number > largest) {
+      largest = number;
+    }
+  }
+  return largest;
+}
+
+console.log(largestNumberFromArray([250, 20, 5, 732, 64, 560]));
