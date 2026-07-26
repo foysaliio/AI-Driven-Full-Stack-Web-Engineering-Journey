a4c8b8e6b3cb93fcc19184701ea8187b68165608
Foysal Hossien <foysalhossien77@gmail.com>
2026-07-26 23:53:55 +0600

    feat(js): Check if a Number is Even or Odd

--- Java Script/Basic Problem Solving with JavaScript/problem-1.js
+++ Java Script/Basic Problem Solving with JavaScript/problem-1.js
@@ -1,1 +1,13 @@
 // Problem 1: Check if a Number is Even or Odd
+
+function checkEvenOrOdd(number) {
+  if (number % 2 === 0) {
+    return "Even number";
+  } else {
+    return "Odd number";
+  }
+}
+
+console.log(checkEvenOrOdd(25));
+console.log(checkEvenOrOdd(26));
+console.log(checkEvenOrOdd(501));
