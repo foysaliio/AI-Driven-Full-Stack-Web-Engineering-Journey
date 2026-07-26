128ff0500d19f45dbca7841c4b66c6b93ad67718
Foysal Hossien <foysalhossien77@gmail.com>
2026-07-27 03:57:21 +0600

    feat(js): Loop Through an Object's Propertie

--- Java Script/Basic Problem Solving with JavaScript/problem-13.js
+++ Java Script/Basic Problem Solving with JavaScript/problem-13.js
@@ -1,3 +1,25 @@
 // Problem 13: Loop Through an Object's Properties
 
+let monitor = {
+  color: "Black",
+  brand: "LG",
+  display: "Something",
+  size: "Something",
+  test: "Dummy",
+};
 
+console.log(monitor["color"]); // bracket notation
+console.log(monitor.brand); // dot notation
+
+// For in loop
+for (let key in monitor) {
+  console.log(key, ": ", monitor[key]);
+}
+
+// Using keys method
+let objKeys = Object.keys(monitor);
+console.log(objKeys);
+
+for (let key of objKeys) {
+  console.log(key, ": ", monitor[key]);
+}
