f9f1aef872e5f3f69a8f8dbb223ef2c911cb4c1b
Foysal Hossien <foysalhossien77@gmail.com>
2026-07-27 03:07:38 +0600

    feat(js): Filter Numbers Greater Than a Value

--- Java Script/Basic Problem Solving with JavaScript/problem-12.js
+++ Java Script/Basic Problem Solving with JavaScript/problem-12.js
@@ -1,2 +1,16 @@
 // Problem 12: Filter Numbers Greater Than a Value
 
+function getFilteredArray(arr, value) {
+  let filteredArray = [];
+
+  for (const number of arr) {
+    if (number > value) {
+      filteredArray.push(number);
+    }
+  }
+
+  return filteredArray;
+}
+
+console.log(getFilteredArray([10, 20, 30, 40, 50, 60, 70, 80, 90, 100], 80));
+console.log(getFilteredArray([10, 20, 30, 40, 50, 60, 70, 80, 90, 100], 10));
