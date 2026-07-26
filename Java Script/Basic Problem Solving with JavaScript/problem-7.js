be7c454ffc18ff95adcff7443f06b221842bfb30
Foysal Hossien <foysalhossien77@gmail.com>
2026-07-27 00:38:26 +0600

    feat(js):Check if a String is a Palindrome

--- Java Script/Basic Problem Solving with JavaScript/problem-7.js
+++ Java Script/Basic Problem Solving with JavaScript/problem-7.js
@@ -1,2 +1,19 @@
 // Problem 7: Check if a String is a Palindrome
 
+function isPalindrome(word) {
+  let reverseStr = "";
+  for (let i = word.length - 1; i >= 0; i--) {
+    let char = word[i];
+    reverseStr += char;
+  }
+  if (word === reverseStr) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(isPalindrome("deed"));
+console.log(isPalindrome("right"));
+console.log(isPalindrome("madam"));
+console.log(isPalindrome("Foysal"));
+console.log(isPalindrome("civic"));
