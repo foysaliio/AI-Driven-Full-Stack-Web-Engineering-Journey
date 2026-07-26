3f277298406b980ed3677087be2a4fc03b1f30b9
Foysal Hossien <foysalhossien77@gmail.com>
2026-07-27 00:27:14 +0600

    feat(js): Reverse a String

--- Java Script/Basic Problem Solving with JavaScript/problem-5.js
+++ Java Script/Basic Problem Solving with JavaScript/problem-5.js
@@ -1,2 +1,14 @@
 // Problem 5: Reverse a String
 
+function reverseWord(word) {
+  let reverseWord = "";
+
+  for (let i = word.length - 1; i >= 0; i--) {
+    reverseWord += word[i];
+  }
+  return reverseWord;
+}
+
+console.log(reverseWord("Programming"));
+console.log(reverseWord("Bangladesh"));
+console.log(reverseWord("Foysal Hossien"));
