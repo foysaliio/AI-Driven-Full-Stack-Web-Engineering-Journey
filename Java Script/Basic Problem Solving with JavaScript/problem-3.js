f6a03d11a7af546e6c77ebcecef54e4fff577c6a
Foysal Hossien <foysalhossien77@gmail.com>
2026-07-26 23:59:32 +0600

    feat(js): find a factorial of a Number

--- Java Script/Basic Problem Solving with JavaScript/problem-3.js
+++ Java Script/Basic Problem Solving with JavaScript/problem-3.js
@@ -1,2 +1,15 @@
 // Problem 3: Factorial of a Number
 
+function getFactorial(number) {
+  let result = 1;
+  for (let i = 1; i <= number; i++) {
+    result = result * i;
+  }
+  return result;
+}
+
+let factorialOfFour = getFactorial(4);
+let factorialOfThree = getFactorial(3);
+
+console.log(factorialOfFour);
+console.log(factorialOfThree);
