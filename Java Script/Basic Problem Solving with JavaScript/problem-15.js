a00ec57f109e457eb7706057d514aba2077afc99
Foysal Hossien <foysalhossien77@gmail.com>
2026-07-27 04:03:33 +0600

    feat(js): Return All Even Numbers, Then Their Sum

--- Java Script/Basic Problem Solving with JavaScript/problem-15.js
+++ Java Script/Basic Problem Solving with JavaScript/problem-15.js
@@ -1,3 +1,27 @@
 // Problem 15: Return All Even Numbers, Then Their Sum
 
 let arr = [10, 20, 50, 150, 6, 7, 9, 11, 23];
+
+function getEvenNumbers(arr) {
+  let evenNumbers = [];
+  for (const number of arr) {
+    if (number % 2 === 0) {
+      evenNumbers.push(number);
+    }
+  }
+  return evenNumbers;
+}
+
+function getSum(arr) {
+  let sum = 0;
+  for (let key of arr) {
+    console.log(key);
+    sum += key;
+  }
+
+  return sum;
+}
+
+let evenNumbers = getEvenNumbers(arr);
+let sum = getSum(evenNumbers);
+console.log("sum: ", sum);
