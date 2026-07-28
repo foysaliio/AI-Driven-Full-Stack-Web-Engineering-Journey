005ebd4ee72b413884ad7f0b356b8b7575f5e6bc
Foysal Hossien <foysalhossien77@gmail.com>
2026-07-29 03:09:44 +0600

    feat(js): create a function that remove duplicate values from an array

---
+++ Java Script/Javascript Practice Task/Task 111 - Remove Duplicate Values from an Array/app.js
@@ -0,0 +1,18 @@
+// Input: an array of numbers
+// Output: array with duplicates removed
+// Returns: an array
+
+function removeDuplicates(numbers) {
+  let unique = [];
+  for (const num of numbers) {
+    if (unique.includes(num)) {
+      continue;
+    } else {
+      unique.push(num);
+    }
+  }
+
+  return unique;
+}
+
+console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5])); // Expected: [1, 2, 3, 4, 5]
