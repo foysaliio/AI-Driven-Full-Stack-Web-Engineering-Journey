0bd9f4544260be23a91360962fd65058eb265eb7
Foysal Hossien <foysalhossien77@gmail.com>
2026-07-29 03:22:20 +0600

    feat(js): create invert an object function

---
+++ Java Script/Javascript Practice Task/Task 112 - Invert an Object/app.js
@@ -0,0 +1,16 @@
+// Input: an object (values are unique)
+// Output: a new object with keys and values swapped
+// Returns: an object
+
+function invertObject(obj) {
+  let inverted = {};
+
+  for (const key in obj) {
+    inverted[obj[key]] = key;
+  }
+
+  return inverted;
+}
+
+console.log(invertObject({ a: 1, b: 2, c: 3 }));
+// Expected: { 1: "a", 2: "b", 3: "c" }
