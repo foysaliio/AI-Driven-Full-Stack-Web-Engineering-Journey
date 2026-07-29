d46561674b5068a585628c1246cc49aaa2079148
Foysal Hossien <foysalhossien77@gmail.com>
2026-07-29 17:41:02 +0600

    fix(js): debug string repeat function

---
+++ Java Script/Javascript Practice Task/Task 123 - String Repeat/app.js
@@ -0,0 +1,11 @@
+function repeatString(str, times) {
+  let result = "";
+
+  for (let i = 0; i < times; i++) {
+    result += str; // find the bug
+  }
+
+  return result;
+}
+
+console.log(repeatString("ab", 3)); // Expected: "ababab"
