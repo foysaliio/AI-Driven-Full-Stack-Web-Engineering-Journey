7698d891d76fa0d8fc31c23b9f2b481492a8f81d
Foysal Hossien <foysalhossien77@gmail.com>
2026-08-06 19:26:43 +0600

    feat(js): implement employee promotion utility

---
+++ Java Script/Problem Solving with Javascript ES6/Practice Task/Task 6. Promote Employee/app.js
@@ -0,0 +1,15 @@
+const promoteEmployee = (employee, newTitle) => {
+  return {
+    ...employee,
+    title: newTitle,
+  };
+};
+
+const employee = {
+  name: "Rafi",
+  title: "Junior Dev",
+};
+
+console.log(promoteEmployee(employee, "Senior Dev"));
+
+console.log(employee);
