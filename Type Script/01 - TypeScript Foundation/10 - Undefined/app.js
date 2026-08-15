17a97bd8233012d94684d92ca7cf21461b6e45e5
Foysal Hossien <foysalhossien77@gmail.com>
2026-08-15 22:32:47 +0600

    feat(typescript): add undefined type example

---
+++ Type Script/01 - TypeScript Foundation/10 - Undefined/app.js
@@ -0,0 +1,8 @@
+"use strict";
+// undefined represents a value that has not been assigned.
+let result = undefined;
+let myPendingMoney;
+let isGasAvailable;
+console.log(result);
+console.log(myPendingMoney);
+console.log(isGasAvailable);
---
+++ Type Script/01 - TypeScript Foundation/10 - Undefined/app.ts
@@ -0,0 +1,11 @@
+// undefined represents a value that has not been assigned.
+
+let result: undefined = undefined;
+
+let myPendingMoney: undefined;
+
+let isGasAvailable: undefined;
+
+console.log(result);
+console.log(myPendingMoney);
+console.log(isGasAvailable);
