bf62830404d3383a24d2ae94063c19d7e487d65a
Foysal Hossien <foysalhossien77@gmail.com>
2026-08-15 22:29:36 +0600

    feat(typescript): add null type example

---
+++ Type Script/01 - TypeScript Foundation/09 - Null/app.js
@@ -0,0 +1,8 @@
+"use strict";
+// null represents an intentional absence of a value.
+let selectedUser = null;
+let rightNowMyPocketMoney = null;
+let mySemesterFinalCgpa = null;
+console.log(selectedUser);
+console.log(rightNowMyPocketMoney);
+console.log(mySemesterFinalCgpa);
---
+++ Type Script/01 - TypeScript Foundation/09 - Null/app.ts
@@ -0,0 +1,11 @@
+// null represents an intentional absence of a value.
+
+let selectedUser: null = null;
+
+let rightNowMyPocketMoney: null = null;
+
+let mySemesterFinalCgpa: null = null;
+
+console.log(selectedUser);
+console.log(rightNowMyPocketMoney);
+console.log(mySemesterFinalCgpa);
