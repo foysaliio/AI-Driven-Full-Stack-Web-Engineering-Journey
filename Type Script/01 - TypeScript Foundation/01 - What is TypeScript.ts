61918200ff978a0160d16fed72fb99ee2a4934ae
Foysal Hossien <foysalhossien77@gmail.com>
2026-08-15 18:52:07 +0600

    feat(typescript): add introduction to TypeScript

---
+++ Type Script/01 - TypeScript Foundation/01 - What is TypeScript.ts
@@ -0,0 +1,10 @@
+// TypeScript is a superset of JavaScript that adds a static type system.
+// It helps developers catch type-related errors during development.
+
+// TypeScript code is compiled into JavaScript before running in the browser.
+
+// Example:
+
+let age: number = 22;
+
+console.log(age);
