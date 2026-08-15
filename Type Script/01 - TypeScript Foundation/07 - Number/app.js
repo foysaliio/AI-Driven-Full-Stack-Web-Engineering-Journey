ffaa3213e6695154097246425a21b7a61130537e
Foysal Hossien <foysalhossien77@gmail.com>
2026-08-15 22:19:00 +0600

    feat(typescript): add number type examples

---
+++ Type Script/01 - TypeScript Foundation/07 - Number/app.js
@@ -0,0 +1,12 @@
+"use strict";
+// The number type is used for both integers and decimal numbers.
+let age = 22;
+let score = 95;
+let price = 499.99;
+let tuhinAge = 22;
+const myFavNum = 77;
+console.log(age);
+console.log(score);
+console.log(price);
+console.log(tuhinAge);
+console.log(myFavNum);
---
+++ Type Script/01 - TypeScript Foundation/07 - Number/app.ts
@@ -0,0 +1,20 @@
+// The number type is used for both integers and decimal numbers.
+
+let age: number = 22;
+
+let score: number = 95;
+
+let price: number = 499.99;
+
+let tuhinAge: number = 22;
+
+const myFavNum: number = 77;
+
+console.log(age);
+
+console.log(score);
+
+console.log(price);
+
+console.log(tuhinAge);
+console.log(myFavNum);
