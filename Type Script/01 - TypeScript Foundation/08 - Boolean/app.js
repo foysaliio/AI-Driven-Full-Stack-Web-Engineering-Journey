7dafd4a626103a9072989a38758da12db8fca9c9
Foysal Hossien <foysalhossien77@gmail.com>
2026-08-15 22:21:06 +0600

    feat(typescript): add boolean type examples

---
+++ Type Script/01 - TypeScript Foundation/08 - Boolean/app.js
@@ -0,0 +1,10 @@
+"use strict";
+// A boolean can only contain true or false.
+let isLoggedIn = true;
+let isAdmin = false;
+let isStudent = true;
+let isRich = false;
+console.log(isLoggedIn);
+console.log(isAdmin);
+console.log(isStudent);
+console.log(isRich);
---
+++ Type Script/01 - TypeScript Foundation/08 - Boolean/app.ts
@@ -0,0 +1,16 @@
+// A boolean can only contain true or false.
+
+let isLoggedIn: boolean = true;
+
+let isAdmin: boolean = false;
+
+let isStudent: boolean = true;
+
+let isRich: boolean = false;
+
+console.log(isLoggedIn);
+
+console.log(isAdmin);
+
+console.log(isStudent);
+console.log(isRich);
