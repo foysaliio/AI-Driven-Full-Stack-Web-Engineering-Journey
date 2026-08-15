fb18e9cb08df95ac1b41224ac9b10c2f3a48624d
Foysal Hossien <foysalhossien77@gmail.com>
2026-08-15 20:44:25 +0600

    feat(typescript): add primitive type examples

---
+++ Type Script/01 - TypeScript Foundation/05 - Primitive Types/app.js
@@ -0,0 +1,13 @@
+"use strict";
+// Primitive types represent the basic types of values in TypeScript.
+// Common primitive types include:
+// string
+// number
+// boolean
+// null
+// undefined
+let username = "Foysal";
+let age = 22;
+let isStudent = true;
+let selectedUser = null;
+let result = undefined;
---
+++ Type Script/01 - TypeScript Foundation/05 - Primitive Types/app.ts
@@ -0,0 +1,18 @@
+// Primitive types represent the basic types of values in TypeScript.
+
+// Common primitive types include:
+// string
+// number
+// boolean
+// null
+// undefined
+
+let username: string = "Foysal";
+
+let age: number = 22;
+
+let isStudent: boolean = true;
+
+let selectedUser: null = null;
+
+let result: undefined = undefined;
