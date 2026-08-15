0d5a45b0572571eb54279ebbed586ac83a83337d
Foysal Hossien <foysalhossien77@gmail.com>
2026-08-15 20:36:17 +0600

    feat(typescript): add type annotation examples

---
+++ Type Script/01 - TypeScript Foundation/03 - Type Annotation.ts/app.js
@@ -0,0 +1,6 @@
+"use strict";
+// Type annotation means explicitly specifying the type of a value.
+let username = "Foysal";
+let age = 22;
+let isStudent = true;
+// The types are explicitly defined.
---
+++ Type Script/01 - TypeScript Foundation/03 - Type Annotation.ts/app.ts
@@ -0,0 +1,9 @@
+// Type annotation means explicitly specifying the type of a value.
+
+let username: string = "Foysal";
+
+let age: number = 22;
+
+let isStudent: boolean = true;
+
+// The types are explicitly defined.
