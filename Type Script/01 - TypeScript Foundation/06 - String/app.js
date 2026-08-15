e230328554f495bf6d8beb61dc2a7caa0942a5b2
Foysal Hossien <foysalhossien77@gmail.com>
2026-08-15 22:16:18 +0600

    feat(typescript): add string type examples

---
+++ Type Script/01 - TypeScript Foundation/06 - String/app.js
@@ -0,0 +1,10 @@
+"use strict";
+// A string represents text.
+const firstName = "Foysal";
+const lastName = "Hossien";
+const country = "Bangladesh";
+const age = "23";
+console.log(firstName);
+console.log(lastName);
+console.log(country);
+console.log(age);
---
+++ Type Script/01 - TypeScript Foundation/06 - String/app.ts
@@ -0,0 +1,13 @@
+// A string represents text.
+
+const firstName: string = "Foysal";
+const lastName: string = "Hossien";
+
+const country: string = "Bangladesh";
+const age: string = "23";
+
+console.log(firstName);
+console.log(lastName);
+
+console.log(country);
+console.log(age);
