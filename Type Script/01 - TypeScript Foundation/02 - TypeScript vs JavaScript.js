e78c84768e890a1843e87de232e8ea14cc3aeddc
Foysal Hossien <foysalhossien77@gmail.com>
2026-08-15 19:13:42 +0600

    feat(typescript): explain TypeScript vs JavaScript

---
+++ Type Script/01 - TypeScript Foundation/02 - TypeScript vs JavaScript.js
@@ -0,0 +1,8 @@
+"use strict";
+// JavaScript allows a variable to hold different types of values.
+let jsAge = 22;
+// jsAge = "Foysal"; // That provide an error because it's typeScript file. for javaScript file this code is ok. anything will be ok for change any datatype.
+// TypeScript allows us to define the expected type.
+let tsAge = 22;
+// This will produce a TypeScript error:
+// tsAge = "Foysal";
---
+++ Type Script/01 - TypeScript Foundation/02 - TypeScript vs JavaScript.ts
@@ -0,0 +1,13 @@
+// JavaScript allows a variable to hold different types of values.
+
+let jsAge = 22;
+
+// jsAge = "Foysal"; // That provide an error because it's typeScript file. for javaScript file this code is ok. anything will be ok for change any datatype.
+
+// TypeScript allows us to define the expected type.
+
+let tsAge: number = 22;
+
+// This will produce a TypeScript error:
+
+// tsAge = "Foysal";
