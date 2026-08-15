984582d0b6ad81ff0f1dcd8e2dab4e5e8e61e8a9
Foysal Hossien <foysalhossien77@gmail.com>
2026-08-15 22:45:48 +0600

    feat(typescript): add unknown type example

---
+++ Type Script/01 - TypeScript Foundation/12 - Unknown/app.js
@@ -0,0 +1,17 @@
+"use strict";
+// unknown is used when the type of a value is not known yet.
+// It is safer than any because the value must be checked
+// before performing type-specific operations.
+let value = "Hello";
+// TypeScript does not allow string-specific operations
+// until the type is checked.
+if (typeof value === "string") {
+    console.log(value.toUpperCase());
+}
+let age = 'Sadik';
+if (typeof age === "number") {
+    if (age > 20) {
+        age = 30;
+        console.log(age);
+    }
+}
