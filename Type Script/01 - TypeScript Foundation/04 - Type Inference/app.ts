2b1e8b65cf99cc78b60033961dfe5dd08cb8ea69
Foysal Hossien <foysalhossien77@gmail.com>
2026-08-15 20:42:30 +0600

    feat(typescript): add type inference examples

---
+++ Type Script/01 - TypeScript Foundation/04 - Type Inference/app.ts
@@ -0,0 +1,14 @@
+// Type inference means TypeScript automatically determines
+// the type of a variable from its initial value.
+
+let username = "Foysal";
+
+let age = 22;
+
+let isStudent = true;
+
+// TypeScript automatically infers:
+
+// username -> string
+// age -> number
+// isStudent -> boolean
