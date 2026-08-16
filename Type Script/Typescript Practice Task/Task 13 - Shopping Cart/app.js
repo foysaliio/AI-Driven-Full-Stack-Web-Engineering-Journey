8f31a50f462382444ecac12df72bbf9c118a30b7
Foysal Hossien <foysalhossien77@gmail.com>
2026-08-17 01:15:23 +0600

    feat(ts): add shopping cart object

---
+++ Type Script/Typescript Practice Task/Task 13 - Shopping Cart/app.js
@@ -0,0 +1,9 @@
+"use strict";
+const shoppingCart = {
+    productName: "Laptop",
+    price: 45000,
+    quantity: 2,
+};
+console.log(`Product: ${shoppingCart.productName}`);
+console.log(`Price: ${shoppingCart.price}`);
+console.log(`Quantity: ${shoppingCart.quantity}`);
---
+++ Type Script/Typescript Practice Task/Task 13 - Shopping Cart/app.ts
@@ -0,0 +1,13 @@
+const shoppingCart: {
+  productName: string;
+  price: number;
+  quantity: number;
+} = {
+  productName: "Laptop",
+  price: 45000,
+  quantity: 2,
+};
+
+console.log(`Product: ${shoppingCart.productName}`);
+console.log(`Price: ${shoppingCart.price}`);
+console.log(`Quantity: ${shoppingCart.quantity}`);
