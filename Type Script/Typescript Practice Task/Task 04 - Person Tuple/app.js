cd64273181e0abac8fa5cb6c387f272fd2776847
Foysal Hossien <foysalhossien77@gmail.com>
2026-08-16 21:26:03 +0600

    feat(ts): add person tuple

---
+++ Type Script/Typescript Practice Task/Task 04 - Person Tuple/app.js
@@ -0,0 +1,3 @@
+"use strict";
+const user = ["Amina", 22];
+console.log(`${user[0]} is ${user[1]} years old.`);
---
+++ Type Script/Typescript Practice Task/Task 04 - Person Tuple/app.ts
@@ -0,0 +1,2 @@
+const user: [string, number] = ["Amina", 22];
+console.log(`${user[0]} is ${user[1]} years old.`);
