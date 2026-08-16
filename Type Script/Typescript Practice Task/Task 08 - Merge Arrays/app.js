9bcfc235c78fb9eae84365129285a01c243cb85c
Foysal Hossien <foysalhossien77@gmail.com>
2026-08-16 23:46:14 +0600

    feat(ts): merge arrays with spread operator

---
+++ Type Script/Typescript Practice Task/Task 08 - Merge Arrays/app.js
@@ -0,0 +1,7 @@
+"use strict";
+const num1 = [1, 2, 3];
+const num2 = [4, 5, 6];
+console.log(num1);
+console.log(num2);
+const mergeNums = [...num1, ...num2];
+console.log(mergeNums);
---
+++ Type Script/Typescript Practice Task/Task 08 - Merge Arrays/app.ts
@@ -0,0 +1,9 @@
+const num1: number[] = [1, 2, 3];
+const num2: number[] = [4, 5, 6];
+
+console.log(num1);
+console.log(num2);
+
+const mergeNums: number[] = [...num1, ...num2];
+
+console.log(mergeNums);
