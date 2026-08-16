b4cb60e7d9a97d37c1aa4947b941f4ca8c68bdf3
Foysal Hossien <foysalhossien77@gmail.com>
2026-08-16 23:25:24 +0600

    feat(ts): add even number checker

---
+++ Type Script/Typescript Practice Task/Task 06 - Even Number Checker/app.js
@@ -0,0 +1,10 @@
+"use strict";
+const isEven = (num) => {
+    if (num % 2 === 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+};
+console.log(isEven(8));
---
+++ Type Script/Typescript Practice Task/Task 06 - Even Number Checker/app.ts
@@ -0,0 +1,9 @@
+const isEven = (num: number): boolean => {
+  if (num % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+console.log(isEven(8));
