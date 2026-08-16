fbe5bdac5b4ed9c37a366814159debbb473a5715
Foysal Hossien <foysalhossien77@gmail.com>
2026-08-16 21:16:56 +0600

    feat(ts): add product information

---
+++ Type Script/Typescript Practice Task/Task 02 - Product Information/app.js
@@ -0,0 +1,5 @@
+"use strict";
+const productName = "Laptop";
+const price = 45000;
+const isAvailable = true;
+console.log(`${productName} cost ${price} and is available: ${isAvailable}`);
---
+++ Type Script/Typescript Practice Task/Task 02 - Product Information/app.ts
@@ -0,0 +1,5 @@
+const productName: string = "Laptop";
+const price: number = 45000;
+const isAvailable: boolean = true;
+
+console.log(`${productName} cost ${price} and is available: ${isAvailable}`);
