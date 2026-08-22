0f301b0a56599b43b2723171ce0a7adb90551dbd
Foysal Hossien <foysalhossien77@gmail.com>
2026-08-22 23:55:46 +0600

    feat(typescript): add class basics

--- Type Script/08 - Object Oriented Programming/01 - Class Basics/app.js
+++ Type Script/08 - Object Oriented Programming/01 - Class Basics/app.js
@@ -1,1 +1,16 @@
 "use strict";
+// A class is a blueprint for creating objects.
+class User {
+    name;
+    age;
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+const user1 = new User("Foysal", 22);
+const user2 = new User("Rahim", 25);
+console.log(user1);
+console.log(user2);
+console.log(user1.name);
+console.log(user2.age);
