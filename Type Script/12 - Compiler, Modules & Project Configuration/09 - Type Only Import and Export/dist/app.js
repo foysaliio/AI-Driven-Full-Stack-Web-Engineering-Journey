89d6a999ddd284e444b2bc13bb930de67ef55da3
Foysal Hossien <foysalhossien77@gmail.com>
2026-08-26 01:24:51 +0600

    feat(typescript): add type only imports and exports

---
+++ Type Script/12 - Compiler, Modules & Project Configuration/09 - Type Only Import and Export/dist/app.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const user = {
+    id: 1,
+    name: "Foysal",
+};
+const role = "admin";
+console.log(user);
+console.log(role);
+//# sourceMappingURL=app.js.map
---
+++ Type Script/12 - Compiler, Modules & Project Configuration/09 - Type Only Import and Export/src/app.ts
@@ -0,0 +1,11 @@
+import type { User, UserRole } from "./types";
+
+const user: User = {
+  id: 1,
+  name: "Foysal",
+};
+
+const role: UserRole = "admin";
+
+console.log(user);
+console.log(role);
