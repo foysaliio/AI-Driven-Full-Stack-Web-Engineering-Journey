3f6a46f16d2e0a88c74663018d0d631cb5576913
Foysal Hossien <foysalhossien77@gmail.com>
2026-08-25 23:01:36 +0600

    docs(typescript): add tsconfig setup

---
+++ Type Script/12 - Compiler, Modules & Project Configuration/02 - Creating tsconfig/app.js
@@ -0,0 +1,20 @@
+"use strict";
+// A TypeScript project can use a tsconfig.json file.
+//
+// Create one with:
+//
+// tsc --init
+//
+// Example:
+//
+// {
+//   "compilerOptions": {
+//     "target": "ES2020",
+//     "module": "ESNext",
+//     "strict": true
+//   }
+// }
+Object.defineProperty(exports, "__esModule", { value: true });
+const message = "TypeScript project configured";
+console.log(message);
+//# sourceMappingURL=app.js.map
---
+++ Type Script/12 - Compiler, Modules & Project Configuration/02 - Creating tsconfig/app.ts
@@ -0,0 +1,19 @@
+// A TypeScript project can use a tsconfig.json file.
+//
+// Create one with:
+//
+// tsc --init
+//
+// Example:
+//
+// {
+//   "compilerOptions": {
+//     "target": "ES2020",
+//     "module": "ESNext",
+//     "strict": true
+//   }
+// }
+
+const message: string = "TypeScript project configured";
+
+console.log(message);
