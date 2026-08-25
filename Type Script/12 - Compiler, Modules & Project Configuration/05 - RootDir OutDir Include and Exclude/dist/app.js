2471ebd18baa8da375be52f88a580699148287a5
Foysal Hossien <foysalhossien77@gmail.com>
2026-08-26 01:02:45 +0600

    docs(typescript): add project directory configuration

---
+++ Type Script/12 - Compiler, Modules & Project Configuration/05 - RootDir OutDir Include and Exclude/dist/app.js
@@ -0,0 +1,22 @@
+"use strict";
+// Example project configuration:
+//
+// {
+//   "compilerOptions": {
+//     "rootDir": "./src",
+//     "outDir": "./dist"
+//   },
+//   "include": ["src"],
+//   "exclude": ["node_modules"]
+// }
+Object.defineProperty(exports, "__esModule", { value: true });
+// rootDir defines where source files are located.
+//
+// outDir defines where compiled files should be created.
+//
+// include defines which files TypeScript should process.
+//
+// exclude defines which files should be ignored.
+const appName = "TypeScript App";
+console.log(appName);
+//# sourceMappingURL=app.js.map
---
+++ Type Script/12 - Compiler, Modules & Project Configuration/05 - RootDir OutDir Include and Exclude/src/app.ts
@@ -0,0 +1,22 @@
+// Example project configuration:
+//
+// {
+//   "compilerOptions": {
+//     "rootDir": "./src",
+//     "outDir": "./dist"
+//   },
+//   "include": ["src"],
+//   "exclude": ["node_modules"]
+// }
+
+// rootDir defines where source files are located.
+//
+// outDir defines where compiled files should be created.
+//
+// include defines which files TypeScript should process.
+//
+// exclude defines which files should be ignored.
+
+const appName: string = "TypeScript App";
+
+console.log(appName);
